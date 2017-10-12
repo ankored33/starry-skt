5f513b7fe45a863f63ab24920f29ab5138d16dd1
Masato Doi <bridgehead33@gmail.com>
2017-10-12 03:58:25 +0000

    install nokogiri

--- main.rb
+++ main.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'nokogiri'
+require 'open-uri'
 
 class StarrySky
   def initialize(skyname)
@@ -19,7 +21,32 @@ class Partner
   def initialize(name)
     @name = name
   end
+
+  def rss()
+    rss = "http://b.hatena.ne.jp/#{@name}/rss"
+  end
+  
+  def nokogiri()
+    opt = {}
+    opt["User-Agent"] = "Opera/9.80 (Windows NT 5.1; U; ja) Presto/2.7.62 Version/11.01 " #User-Agent偽装
+    charset = nil
+    xml = open(rss,opt) do |f|
+      charset = f.charset #文字種別を取得
+      f.read #htmlを読み込んで変数xmlに渡す
+    end
+    doc = Nokogiri::XML(xml)
+    doc.remove_namespaces!
+    titles = doc.search('title')
+    p titles.text
+  end
+  
+  def parse(doc)
+  end
 end
+
+
+partner = Partner.new("barbieri")
+partner.nokogiri
 
 get "/" do
   test = StarrySky.new("test")
